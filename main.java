69ffb004f4f0efd5cf5488e7ee2ff4c08455f5a5
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-01-27 16:49:53 +0530

    swap

---
+++ main.java
@@ -0,0 +1,13 @@
+
+class Main {
+    public static void main(String[] args) {
+         int a =5;
+        int b=6;
+        int c;
+        a=b;
+        c=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
